8ffae4bee1ab42c92a38d892b378895cee169ae2
fbertaux <francois.bertaux@pasteur.fr>
2019-10-30 11:24:56 +0100

    updated the code

--- ERA_model_simulator.py
+++ ERA_model_simulator.py
@@ -1,6 +1,7 @@
 import numpy as np # for basic vector computation in python
 import scipy.integrate as integrate # needed to solve ODEs
 import matplotlib.pyplot as plt # for plotting
+from ipywidgets import interact, interact_manual # interactive widgets
 
 # the function describing the right-hand side of the ODEs
 # y[0] = E, y[1] = R, y[2] = A
@@ -54,3 +55,47 @@ def simulate_conc(params, duration, e0, r0, a0):
     t = ode_result.t
     return t, e, r, a
     
+
+print('Exploration of model behavior (press Run Interact to update)')
+@interact_manual
+def display_model_behavior(fE=(0,1,0.01), k=(0.01,10,0.01), duration=(5,60,5)):
+    params = {'sigma':1, 'k':k, 'a_sat':0.01, 'f_E':fE}    
+    e0, r0, a0 = 0.4, 0.4, 0.2
+    t, e, r, a = simulate_conc(params=params, duration=duration, e0=e0, r0=r0, a0=a0)
+    f, (ax1,ax2) = plt.subplots(ncols=2, nrows=1, figsize=(15,5))
+    ax1.plot(t, e, 'g', label='e')
+    ax1.plot(t, r, 'b', label='r')
+    ax1.plot(t, a, 'm', label='a')
+    ax1.plot(t, e + r + a, '--k')
+    ax1.legend()
+    ax1.set_ylabel('concentration or mass fraction')
+    ax1.set_ylim([0,1])
+    ax2.plot(t, k*e, 'r')
+    ax2.set_ylim([0,1])
+    ax2.set_ylabel('Instantaneous growth rate')
+    ax2.set_title(f'Growth rate at the end: {k*e[-1]:.3f}')
+    for ax in [ax1, ax2]:
+        ax.set_xlabel('Time')    
+    plt.show()
+
+print('Exploration of how the allocation strategy impacts the growth rate (press Run Interact to update)')
+@interact_manual
+def explore_allocation(k=(0.01,10,0.01), duration=(5,60,5)):
+    params = {'sigma':1, 'k':k, 'a_sat':0.01}
+    e0, r0, a0 = 0.4, 0.4, 0.2
+    fE_vec = np.linspace(0,1,21)
+    cmap = plt.cm.get_cmap()
+    f, (ax1,ax2) = plt.subplots(ncols=2, nrows=1, figsize=(15,5))
+    end_growth_rates = []
+    for fE in fE_vec:
+        params['f_E'] = fE
+        color = cmap.colors[np.int(fE*(cmap.N-1))]
+        t, e, r, a = simulate_conc(params=params, duration=duration, e0=e0, r0=r0, a0=a0)
+        end_growth_rates.append(params['k']*e[-1])
+        ax1.plot(t, e, color=color)
+    ax1.set_xlabel('Time')
+    ax1.set_ylabel('E concentration (e)')
+    ax2.plot(fE_vec, end_growth_rates,'-ro')
+    ax2.set_xlabel('fE')
+    ax2.set_ylabel('End growth rate')
+    plt.show()
